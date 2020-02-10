db6f6efe8224e1e616f633734fcf50d8c621ad1a
studenti <studenti@rosselli.loc>
2020-02-10 09:41:54 +0100

    Add project files.

---
+++ Esercizio1_Pag278/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Esercizio1_Pag278
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti numeri vuoi inserire?");
+            int n = int.Parse(Console.ReadLine());
+            List<int> lista = new List<int>(n);
+            List<int> duplicati = new List<int>();
+            for (int i=0; i<n;i++)
+            {
+                Console.WriteLine("Inserisci un numero");
+                int num = int.Parse(Console.ReadLine());
+                if (lista.Contains(num))
+                {
+                    duplicati.Add(num);
+                }
+                else
+                {
+                    lista.Add(num);
+                }
+            }
+            foreach (int doppioni in duplicati)
+                {
+                    Console.WriteLine($"I duplicati sono: {doppioni}");
+                }
+                Console.ReadLine();
+        }
+    }
+}
---
+++ Esercizio2_Pag278/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Esercizio2_Pag278
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti numeri vuoi inserire?");
+            int n = int.Parse(Console.ReadLine());
+            List<int> lista = new List<int>(n);
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                lista.Add(random.Next(1, 1000));
+            }
+            lista.Sort();
+            foreach (int ordine in lista)
+            {
+                Console.WriteLine($"I numeri in ordine crescente sono: {ordine}");
+            }
+            Console.ReadLine();
+        }
+    }
+}

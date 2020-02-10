5adc4ba141b369090c0ad1cab74e17ab7dca9884
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-10 09:53:01 +0100

    inizio 1 pag 294

---
+++ Esercizio1_Pag294/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Esercizio1_Pag294
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]num = new int[30];
+            Random random = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                num[i] = random.Next(1, 1000);
+                if()
+            }
+        }
+    }
+}
